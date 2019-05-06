ca2d64021c581e66460e41b5a8cf4e7fc1232ed2
Nate Boyette <nateboyette@icloud.com>
2019-05-06 14:49:25 -0700

    Created linked list file and imported. Implemented the queue using a single linked list. Receiving an error on one test.

---
+++ queue/linked_list.py
@@ -0,0 +1,61 @@
+class Node:
+    def __init__(self, value, next_node=None):
+        self.value = value
+        self.next_node = next_node
+
+    def get_value(self):
+        return self.value
+
+    def get_next(self):
+        return self.next_node
+
+    def set_next(self, new_next):
+        self.next_node = new_next
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_to_tail(self, value):
+        new_node = Node(value)
+
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
+
+        else:
+            self.tail.set_next(new_node)
+
+            self.tail = new_node
+
+    def remove_head(self):
+
+        if not self.head and not self.tail:
+            return None
+
+        if self.head == self.tail:
+            old_head = self.head
+            self.head = None
+            self.tail = None
+
+            return old_head.get_value()
+        else:
+            old_head = self.head
+            self.head = self.head.get_next()
+            return old_head.get_value()
+
+    def contain(self, value):
+        if not self.head and not self.tail:
+            return None
+
+        current = self.head
+
+        while current:
+            if current.get_value() == value:
+                return True
+
+            current = current.get_next()
+
+        return False
--- queue/queue.py
+++ queue/queue.py
@@ -1,15 +1,50 @@
+from linked_list import LinkedList
+
+
 class Queue:
-  def __init__(self):
-    self.size = 0
-    # what data structure should we
-    # use to store queue elements?
-    self.storage = 
+    def __init__(self):
+        self.size = 0
+        # what data structure should we
+        # use to store queue elements?
+        self.storage = LinkedList()
 
-  def enqueue(self, item):
-    pass
-  
-  def dequeue(self):
-    pass
+    def enqueue(self, item):
+        self.storage.add_to_tail(item)
+        return self.storage.tail.get_value()
 
-  def len(self):
-    pass
+    def dequeue(self):
+        return f"{self.storage.head.get_value()} was removed"
+        self.storage.remove_head()
+
+    def len(self):
+        self.size = 0
+        if not self.storage.head and not self.storage.tail:
+            return 0
+
+        if self.storage.head == self.storage.tail:
+            return 1
+
+        current_value = self.storage.head
+
+        while current_value:
+            self.size += 1
+            current_value = current_value.get_next()
+
+        return self.size
+
+
+q = Queue()
+
+print(q.len())
+q.enqueue(2)
+print(q.len())
+q.enqueue(4)
+print(q.len())
+q.enqueue(6)
+q.enqueue(8)
+q.enqueue(10)
+q.enqueue(12)
+q.enqueue(14)
+q.enqueue(16)
+q.enqueue(18)
+print(q.len())
