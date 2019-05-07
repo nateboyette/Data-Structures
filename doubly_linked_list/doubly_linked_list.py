33c79b6a408aa7f5e7d4f4a7daaa450ac32cb0da
Nate Boyette <nateboyette@icloud.com>
2019-05-07 15:17:05 -0700

    Passing all tests for doubly linked list except for get max, move to end, and move to front.

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -52,25 +52,32 @@ class DoublyLinkedList:
         self.length = 1 if node is not None else 0
 
     def __len__(self):
-
-        if self.head == self.tail:
-            return 1
-
-            return self.length
+        return self.length
 
     def add_to_head(self, value):
         new_node = ListNode(value)
-
         if not self.head and not self.tail:
             self.head = new_node
             self.tail = new_node
+            self.length += 1
         else:
             self.head.insert_before(value)
             self.head = self.head.prev
+            self.length += 1
 
     def remove_from_head(self):
-        new_head = self.head
-        return new_head.get_value()
+        if self.head == self.tail:
+            deleted_value = self.head.value
+            self.head = None
+            self.tail = None
+            self.length -= 1
+            return deleted_value
+        else:
+            deleted_value = self.head.value
+            self.head = self.head.next
+            self.head.prev = None
+            self.length -= 1
+            return deleted_value
 
     def add_to_tail(self, value):
         new_node = ListNode(value)
@@ -78,21 +85,36 @@ class DoublyLinkedList:
         if not self.head and not self.tail:
             self.head = new_node
             self.tail = new_node
+            self.length += 1
         else:
             self.tail.insert_after(value)
             self.tail = self.tail.next
+            self.tail.next = None
+            self.length += 1
 
     def remove_from_tail(self):
-        pass
+        new_tail = self.tail.prev
+        old_tail = self.tail
+        self.tail.delete()
+        self.tail = new_tail
+        self.length -= 1
+        return old_tail.value
 
     def move_to_front(self, node):
-        pass
+        self.add_to_head(node.value)
+        self.delete(node)
 
     def move_to_end(self, node):
-        pass
+        self.add_to_tail(node.value)
 
     def delete(self, node):
-        pass
+        if node.prev is None:
+            node.remove_from_head()
+        elif node.next is None:
+            node.remove_from_tail()
+        else:
+            self.length -= 1
+            node.delete()
 
     def get_max(self):
         pass
@@ -104,8 +126,9 @@ dblList.add_to_head(2)
 dblList.add_to_head(55)
 dblList.add_to_head(60)
 dblList.add_to_tail(20)
-# dblList.add_to_tail(30)
-# dblList.remove_from_head()
+dblList.add_to_tail(30)
+dblList.remove_from_head()
+dblList.remove_from_tail()
 
 print(dblList.head.next.get_value())
 print(dblList.head.get_value())
